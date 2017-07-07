bb4a4ce71567f0d389fb6ab52e2b380248b01651
Kat Solberg <katsolberg@gmail.com>
2017-07-07 15:58:17 -0700

    add gravatar to itemcard

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Card, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import Gravatar from 'react-gravatar'
 
 const ItemCard = ({ itemData }) => (
     <li className="itemCardWrapper">
         <Card>
-            <CardHeader
-                title={itemData.title}
-                subtitle="Subtitle"
-                avatar="images/jsa-128.jpg"
-            />
             <CardMedia>
                 <img src={itemData.imageUrl} alt="" />
             </CardMedia>
+            <CardHeader
+                title={itemData.itemOwner.fullName}
+                subtitle="Subtitle"
+                avatar={<Gravatar email={itemData.itemOwner.email} />}
+            />
+
             <CardTitle title={itemData.title} subtitle="Card subtitle" />
             <CardText>
                 {itemData.description}
